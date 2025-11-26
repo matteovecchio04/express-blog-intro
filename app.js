6e85dd4fd6d4b104bc4675a488495de1b95729c4
matteovecchio04 <matteovex@gmail.com>
2025-11-26 19:31:11 +0100

    add posts obj array

--- app.js
+++ app.js
@@ -2,8 +2,40 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const posts = [
+    {
+        title: "Primo post",
+        content: "Questo è il contenuto del primo post.",
+        image: "",
+        tags: ["first", "blog"]
+    },
+    {
+        title: "Secondo post",
+        content: "Questo è il contenuto del secondo post",
+        image: "",
+        tags: ["second", "blog"]
+    },
+    {
+        title: "Terzo post",
+        content: "Questo è il contenuto del terzo post",
+        image: "",
+        tags: ["third", "blog"]
+    },
+    {
+        title: "Quarto post",
+        content: "Questo è il contenuto del quarto post",
+        image: "",
+        tags: ["fourth", "blog"]
+    },
+    {
+        title: "Quinto post",
+        content: "Questo è il contenuto del quinto post",
+        image: "",
+        tags: ["fifth", "blog"]
+    }]
+
 app.get("/", (req, res) => {
-    res.send("Server del mio blog")
+    res.send(posts)
 })
 
 app.listen(port, () => {
